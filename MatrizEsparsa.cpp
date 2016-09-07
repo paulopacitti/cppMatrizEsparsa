abd3e306bcbfb0892552ba6709897108a8398c16
Paulo Pacitti <ppacitti@outlook.com>
2016-09-07 18:19:50 -0300

    :100:  - 07/09/16
    Tá quase pronto

--- MatrizEsparsa.cpp
+++ MatrizEsparsa.cpp
@@ -6,5 +6,72 @@ MatrizEsparsa:: MatrizEsparsa(int l, int c, int v)
     this->nColunas = c;
     this->valorComum = v;
 
-    this->linhas
+    this->vetLinhas = new Lista*[l-1];
+
+    for(int i=0; i<(l-1); i++)
+    {
+        this->vetLinhas[i] = NULL;
+    }
 }
+
+MatrizEsparsa::~MatrizEsparsa()
+{
+
+}
+
+void MatrizEsparsa::incluir(int l, int c, int v) throw()
+{
+    if(v != valorComum && validaLinhaColuna())
+    {
+        No novaColuna = new No(c, v);
+        if(!vetLinhas[l].existe(novaColuna))
+        {
+            if(vetLinhas[l-1] == NULL)
+            {
+                Lista novaLinha = new Lista(novaColuna);
+                novaLinha.incluir(novaColuna);
+                vetLinhas[l-1] = novaLinha;
+            }
+            else
+                vetLinhas[l-1].incluir(novaColuna);
+        }
+    }
+    else
+        throw "Elemento já existe ou não compreende as dimensões da matriz";
+}
+
+void MatrizEsparsa::excluir(int l, int c, int v) throw()
+{
+    if(v != valorComum && validaLinhaColuna())
+    {
+        No novaColuna = new No(c, v);
+        if(vetLinhas[l-1].existe(novaColuna))
+            vetLinhas[l-1].excluir(novaColuna);
+    }
+    else
+        throw "Elemento não existe na matriz";
+
+}
+
+bool MatrizEsparsa::validaLinhaColuna(int l, int c)
+{
+    if(l <= this->nLinhas && c <= this->nColunas)
+        return true;
+    else
+        return false;
+}
+
+OStream& operator<< (OStream& o, MatrizEsparsa m)
+{
+    for(int i=0; i < m.nLinhas -1; i++)
+    {
+        for(int j = 0; j < m.nColunas -1; i++)
+        {
+            if(m.vetLinhas)
+        }
+    }
+}
+
+
+
+
--- MatrizEsparsa.h
+++ MatrizEsparsa.h
@@ -1,22 +1,27 @@
 #ifndef MATRIZESPARSA
 #define MATRIZESPARSA
 
+
 class MatrizEsparsa
 {
     private:
         int nLinhas;
         int nColunas;
         int valorComum;
+        Lista[] vetLinhas;
 
-        Linha* linhas;
+        bool validaLinhaColuna(int l, int v);
 
     public:
         MatrizEsparsa(int l, int c, int v) const;
         virtual ~MatrizEsparsa();
-        void incluir(int i) throw():
-        void excluir(int i) throw();
+        void incluir(int l, int c, int v) throw():
+        void excluir(int l, int c, int v) throw();
+        friend OStream& operator<< (OStream& o, MatrizEsparsa m);
 
 };
+
+extern OStream& operator<< (OStream& o, MatrizEsparsa m);
 
 #endif
 
